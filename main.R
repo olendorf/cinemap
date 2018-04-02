7b098c5b14a4fab7dcabf2bfec756a8c8aaabf14
Robert Olendorf <drolendorf@gmail.com>
2018-04-02 15:52:45 -0400

    tmdb first pass

--- main.R
+++ main.R
@@ -17,30 +17,37 @@ install_packages <- function(packages) {
   }
 }
 
-
 install_packages(c("rvest", "TMDb"))
 
 api_key_v3 <- '90baee00115159ddf9966b23a1d51062'
 
 master_data <- read.csv('data/master_data.csv', header = TRUE, stringsAsFactors=FALSE)
 
-# query_results_en <- search_movie(api_key=api_key_v3, query="If cats disappeared from the world")
-# query_results_romanized <- search_movie(api_key=api_key_v3, query="sekai kara neko ga kietanara")
-# query_results_original <- search_movie(api_key=api_key_v3, query="世界から猫が消えたなら")
+# If you get HTTP error 429 you are making too many requests too quickly. Increasing
+# the sleep time will fix that once its high enough at the cost of taking longer to
+# retrieve the data.
+sleep_time <- 0.5
+
+japanese_char_regex <-  "[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B/g"
 
 cinemap_brief <- master_data[1:50,]  # This is for development. Remove and substitute master_data later
 
-
 # Do some cleanup
 cinemap_brief[cinemap_brief$release_date == "--",]$release_date = ""
+cinemap_brief[cinemap_brief$production_studio == "--",]$production_studio = ""
+cinemap_brief$production_studio <- gsub("/", "; ", cinemap_brief$production_studio)
 
 # Initialize new columns with default values
-# cinemap_brief$en_hits <- 0
-# cinemap_brief$romanji_hits <- 0
-# cinemap_brief$original_lang_hits <- 0
 cinemap_brief$tmdb_id <- ""
 cinemap_brief$tmdb_poster_url <- ""
+cinemap_brief$genres <- ""
+cinemap_brief$spoken_languages <- ""
+cinemap_brief$imdb_id <- ""
+cinemap_brief$tmdb_cast <- ""
+cinemap_brief$producer <- ""
+cinemap_brief$tmdb_crew <- ""
 
+# Utility functions to DRY up and promote readability
 movie_data <- function(title, year){
   query_result <- list(total_results = 0, results = NULL)
   if(title != ""){
@@ -49,38 +56,118 @@ movie_data <- function(title, year){
   return(query_result)
 }
 
+
+start_time <- Sys.time()
+last_title <- ""
+repeated_title = FALSE
 for(i in 1:length(cinemap_brief$film_title_en)) {
-  result <- movie_data(cinemap_brief[i,]$film_title_en, cinemap_brief[i,]$year_released)
-  Sys.sleep(1)
-  # cinemap_brief[i,]$en_hits <- result$total_results
-  if(result$total_results != 1) {
-    result <- movie_data(cinemap_brief[i,]$film_title_romanji, cinemap_brief[i,]$year_released)
-    # cinemap_brief[i,]$romanji_hits <- result$total_results
-    Sys.sleep(1)
+  if(cinemap_brief[i,]$film_title_en != last_title) {
+    repeated_title = FALSE
+    last_title <- cinemap_brief[i,]$film_title_en  # Don't send requests for duplicate titles
+    print(paste("Working on ", last_title))
+    
+    # Work through the various titles to get a hit.
+    result <- movie_data(cinemap_brief[i,]$film_title_en, cinemap_brief[i,]$year_released)
+    Sys.sleep(sleep_time)
+    if(result$total_results != 1) {
+      result <- movie_data(cinemap_brief[i,]$film_title_romanji, cinemap_brief[i,]$year_released)
+      Sys.sleep(sleep_time)
+    }
+    if(result$total_results != 1) {
+      result <- movie_data(cinemap_brief[i,]$film_title_original, cinemap_brief[i,]$year_released)
+      Sys.sleep(sleep_time)
+    }
   }
-  if(result$total_results != 1) {
-    result <- movie_data(cinemap_brief[i,]$film_title_original, cinemap_brief[i,]$year_released)
-    # cinemap_brief[i,]$original_lang_hits <- result$total_results
-    Sys.sleep(1)
+  else {
+    repeated_title = TRUE
   }
   
+  
   if(result$total_results == 1) {
+    # Get the info returned in the basic query
     cinemap_brief[i,]$tmdb_id <- result$results$id
     if(cinemap_brief[i,]$film_title_original == "") {
-      cinemap_brief[i,]$film_title_original = result$results$original_title
+      cinemap_brief[,]$film_title_original = result$results$original_title
     }
     if(cinemap_brief[i,]$year_released == "") {
-      cinemap_brief[i,]$year_released = regmatches(result$results$release_date, regexpr("[0-9]{4}", result$results$release_date))
+      cinemap_brief[,]$year_released = regmatches(result$results$release_date, regexpr("[0-9]{4}", result$results$release_date))
     }
     if(cinemap_brief[i,]$release_date == "") {
-      cinemap_brief[i,]$release_date = result$results$release_date
+      cinemap_brief[,]$release_date = result$results$release_date
     }
     if(cinemap_brief[i,]$tmdb_poster_url == "") {
-      cinemap_brief[i,]$tmdb_poster_url = result$results$poster_path
-      print(cinemap_brief[i,]$tmdb_poster_url)
+      cinemap_brief[,]$tmdb_poster_url = result$results$poster_path
     }
+    
+    # Get the movie details provided if it hasn't been retrieved already
+    if(!repeated_title) {
+      details <- movie(api_key = api_key_v3, result$results$id)
+      Sys.sleep(sleep_time)
+    }
+    if(cinemap_brief[i,]$genres == "") {
+      cinemap_brief[,]$genres = paste(details$genres$name, collapse = "; ")
+    }
+    if(cinemap_brief[i,]$spoken_languages == "") {
+      cinemap_brief[,]$spoken_languages = paste(details$spoken_languages$iso_639_1, collapse = "; ")
+    }
+    if(cinemap_brief[i,]$production_studio == "") {
+      cinemap_brief[,]$production_studio = paste(details$production_companies$name, collapse = "; ")
+    }
+    if(cinemap_brief[i,]$country_of_production == "") {
+      cinemap_brief[,]$country_of_production = paste(details$production_countries$iso_3166_1, collapse = "; ")
+    }
+    if(cinemap_brief[i,]$imdb_id == "") {
+      cinemap_brief[,]$imdb_id = details$imdb_id
+    }
+    if(cinemap_brief[i,]$budget == "") {
+      cinemap_brief[,]$budget = details$budget
+    }
+    
+    
+    # Get alt title info and update it if needed.
+    if(!repeated_title)
+    {
+      alt_titles <- movie_alternative_title(api_key = api_key_v3, id = result$results$id)
+      Sys.sleep(sleep_time)
+    }
+    if(cinemap_brief[1,]$film_title_romanji == "" || cinemap_brief[1,]$film_title_original == "")
+    {
+      # alt_titles <- movie_alternative_title(api_key = api_key_v3, id = result$results$id)
+      # Sys.sleep(sleep_time)
+      alt_titles <- alt_titles$titles$title[alt_titles$titles$iso_3166_1 == "JP"]
+      if(cinemap_brief[i,]$film_title_romanji == "") {
+        cinemap_brief[,]$film_title_romanji = alt_titles[!grepl(japanese_char_regex, alt_titles)]
+      }
+      if(cinemap_brief[i,]$film_title_original == "") {
+        cinemap_brief[,]$film_title_original = alt_titles[grepl(japanese_char_regex, alt_titles)]
+      }
+    }
+    
+    # Get cast and crew
+    if(!repeated_title)
+    {
+      cast_and_crew <- movie_credits(api_key = api_key_v3, id = result$results$id)
+      Sys.sleep(sleep_time)
+    }
+
+    if(cinemap_brief[i,]$tmdb_cast == "") {
+      cinemap_brief[i,]$tmdb_cast <- toJSON(cast_and_crew$cast)
+    }
+    if(cinemap_brief[i,]$director == "") {
+      cinemap_brief[i,]$director <- paste(cast_and_crew$crew[ which(cast_and_crew$crew$job == "Director"),]$name, collapse =  "; ")
+    }
+    if(cinemap_brief[i,]$producer == "") {
+      cinemap_brief[i,]$producer <- paste(cast_and_crew$crew[ which(cast_and_crew$crew$job == "Producer"),]$name, collapse =  "; ")
+    }
+    if(cinemap_brief[i,]$tmdb_crew == "") {
+      cinemap_brief[i,]$tmdb_crew <- toJSON(cast_and_crew$crew)
+    }
+    
   }
 }
+end_time <- Sys.time()
+
+print(end_time - start_time)
 
 #regmatches(string, regexpr(pattern, string)) 
 
